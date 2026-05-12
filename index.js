f25d0397236de61eb64a3e8c414cc7ec35188865
serenity <serenitydedman@gmail.com>
2026-05-12 16:19:26 +0000

    updated index js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let distanceInMiles = 100;
 
+let estimatedArrivalTime = () {
+    let milesPerHour = 50;
+    return distanceInMiles / estimatedArrivalTime;
+}
+
+
+document.getElementById("result").innerHTML = estimatedArrivalTime();
